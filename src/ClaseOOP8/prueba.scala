d231db4e4949ae75bd43ee2f6524f73d22c0e042
Adam Casas <mipeeeerro@gmail.com>
2024-03-11 17:56:24 +0100

    .

---
+++ src/ClaseOOP8/prueba.scala
@@ -0,0 +1,22 @@
+package ClaseOOP8
+
+// Un case class es una clase que ya implementa por defecto una serie de funcionalidades
+// y tiene un object companion
+case class prueba(nombre:String, edad:Int)
+
+
+object programa {
+  def main(args: Array[String]): Unit = {
+
+    //El case class implementa un apply, enotnces no hace falta usar la clausula new
+    var instancia_1 = prueba("jaime",20)
+
+    //hashcode(), equals() y copy() son ejemplos de metodos
+    // implementados por defecto en una case class
+    println(instancia_1.hashCode())
+    val instancia_2 = instancia_1.copy(nombre= "Ribera", edad = 30)
+    println(instancia_1.equals(instancia_2))
+
+    println(instancia_1)
+  }
+}
